1a7972f35b4ca15ae419dcb0ffafabb1cea5049b
JuanMorenoG123 <juan.moreno@designingsolutions.com.co>
2025-03-05 14:34:15 -0600

    Cambios

--- database/migrations/2025_03_05_163458_add_columns_size_etx_to_files.php
+++ database/migrations/2025_03_05_163458_add_columns_size_etx_to_files.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('files', function (Blueprint $table) {
             $table->string('size')->nullable();
-            $table->string('etx')->nullable();
+            $table->string('ext')->nullable();
         });
     }
 
@@ -24,7 +24,7 @@ return new class extends Migration
     {
         Schema::table('files', function (Blueprint $table) {
             $table->dropColumn('size');
-            $table->dropColumn('etx');
+            $table->dropColumn('ext');
         });
     }
 };
