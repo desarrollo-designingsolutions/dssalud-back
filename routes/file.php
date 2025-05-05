9d6ba83a23688e4885c6c029a17f65216f8a61a6
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-05-05 10:12:29 -0500

    Cambios funcionales

--- routes/file.php
+++ routes/file.php
@@ -28,3 +28,7 @@ Route::post('/file/massUpload', [FileController::class, 'massUpload']);
 Route::get('/file/paginate', [FileController::class, 'paginate']);
 
 Route::get('/file/getUrlS3', [FileController::class, 'getUrlS3']);
+
+
+
+//hola
