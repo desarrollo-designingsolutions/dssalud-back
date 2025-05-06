0a93832783d31cfbf6d388fd4d93300733f51e10
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-05-06 13:12:52 -0500

    Cambios funcionales

--- app/Repositories/FileRepository.php
+++ app/Repositories/FileRepository.php
@@ -41,38 +41,38 @@ class FileRepository extends BaseRepository
     {
         $cacheKey = $this->cacheService->generateKey("{$this->model->getTable()}_paginate", $request, 'string');
 
-        // return $this->cacheService->remember($cacheKey, function () use ($request) {
+        return $this->cacheService->remember($cacheKey, function () use ($request) {
 
-        $query = QueryBuilder::for($this->model->query())
-            ->allowedFilters([
-                AllowedFilter::callback('inputGeneral', function ($query, $value) use ($request) {
-                    $query->where(function ($query) use ($value, $request) {
-                        $query->orWhere('filename', 'like', "%$value%");
+            $query = QueryBuilder::for($this->model->query())
+                ->allowedFilters([
+                    AllowedFilter::callback('inputGeneral', function ($query, $value) use ($request) {
+                        $query->where(function ($query) use ($value, $request) {
+                            $query->orWhere('filename', 'like', "%$value%");
 
-                        $query->orWhereHas('supportType', function ($subQuery) use ($value, $request) {
-                            $subQuery->where('name', 'like', "%$value%");
+                            $query->orWhereHas('supportType', function ($subQuery) use ($value, $request) {
+                                $subQuery->where('name', 'like', "%$value%");
+                            });
                         });
-                    });
-                }),
-            ])
-            ->allowedSorts([
-                'observation',
-            ])
-            ->where(function ($query) use ($request) {
-                if (isset($request['company_id']) && ! empty($request['company_id'])) {
-                    $query->where('company_id', $request['company_id']);
-                }
-                if (! empty($request['fileable_id'])) {
-                    $query->where('fileable_id', $request['fileable_id']);
-                }
-                if (! empty($request['fileable_type'])) {
-                    $query->where('fileable_type', 'App\\Models\\' . $request['fileable_type']);
-                }
-            })
-            ->paginate(request()->perPage ?? Constants::ITEMS_PER_PAGE);
+                    }),
+                ])
+                ->allowedSorts([
+                    'observation',
+                ])
+                ->where(function ($query) use ($request) {
+                    if (isset($request['company_id']) && ! empty($request['company_id'])) {
+                        $query->where('company_id', $request['company_id']);
+                    }
+                    if (! empty($request['fileable_id'])) {
+                        $query->where('fileable_id', $request['fileable_id']);
+                    }
+                    if (! empty($request['fileable_type'])) {
+                        $query->where('fileable_type', 'App\\Models\\' . $request['fileable_type']);
+                    }
+                })
+                ->paginate(request()->perPage ?? Constants::ITEMS_PER_PAGE);
 
-        return $query;
-        // }, Constants::REDIS_TTL);
+            return $query;
+        }, Constants::REDIS_TTL);
     }
 
     public function store($request)
